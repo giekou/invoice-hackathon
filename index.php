12cccfa1bd6b9ae3feeb16a4083f02aa28218246
Iscar Sam <iscargx@yahoo.co.id>
2022-03-12 01:49:35 +0700

    set ui in manage page and fix index.php

--- index.php
+++ index.php
@@ -5,19 +5,22 @@
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Import materialize.css-->
       <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+      <link type="text/css" rel="stylesheet" href="css/custom.css"  media="screen,projection"/>
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    </head>
 
-    <body>
+
+
 
       <!--JavaScript at end of body for optimized loading-->
       <script type="text/javascript" src="js/materialize.min.js"></script>
-    </body>
-  </html>
+      <!-- <script type="text/javascript" src="js/page.js"></script> -->
 
-<body>
+  
+
+
 <nav>
     <div class="nav-wrapper blue">
       <a href="create-invoice.php" class="brand-logo center">Blibli Invoice System</a>
@@ -28,4 +31,5 @@
       </ul>
     </div>
   </nav>
-</body>
+</head>
+<!-- </html> -->
